d21c8d16bfcb7967f963316064742567f6d80ef5
Kamatcho <mohamed.phpstorm@gmail.com>
2018-10-29 00:54:39 +0200

    Initial commit

---
+++ OcrTest/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  OcrTest
+//
+//  Created by MOHAMED on 9/10/17.
+//  Copyright © 2017 MOHAMED. All rights reserved.
+//
+
+import UIKit
+import TesseractOCR
+class ViewController: UIViewController , G8TesseractDelegate{
+
+    @IBOutlet weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let tesseract = G8Tesseract(language: "eng") {
+            tesseract.delegate = self
+            tesseract.image = UIImage(named: "demoText")?.g8_blackAndWhite()
+            tesseract.recognize()
+            
+            textView.text = tesseract.recognizedText
+            
+        }
+        
+        
+    }
+    
+    func progressImageRecognition(for tesseract: G8Tesseract!) {
+        print("Recognition Progress \(tesseract.progress) %")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
